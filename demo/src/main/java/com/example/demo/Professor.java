33e1fff8614d6ad8718a4f176879ce5c85bfd71d
paulo-deodato <10310189@mackenzista.com.br>
2024-09-27 01:38:34 +0000

    add professor

--- demo/src/main/java/com/example/demo/Professor.java
+++ demo/src/main/java/com/example/demo/Professor.java
@@ -1,4 +1,4 @@
-package com.example.professor;
+package com.example.demo;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- demo/src/main/java/com/example/demo/ProfessorController.java
+++ demo/src/main/java/com/example/demo/ProfessorController.java
@@ -1,4 +1,4 @@
-package com.example.professor;
+package com.example.demo;
 
 import java.util.Optional;
 
