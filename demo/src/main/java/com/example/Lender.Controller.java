c3ddcbd9bfeb71d3553ec5e42172c93701b169f1
Amanda Massari <54778522+amandaskywalker@users.noreply.github.com>
2024-10-11 01:27:51 +0000

    fase3

---
+++ demo/src/main/java/com/example/Lender.Controller.java
@@ -0,0 +1,57 @@
+package com.example;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+class LenderController {
+
+    @Autowired
+    private LenderRepo lenderRepo;
+
+    public LenderController() {
+    }
+
+    @GetMapping("/api/lenders")
+    Iterable<Lender> getLenders(@RequestParam Optional<String> lenderType) {
+        return lenderRepo.findAll();
+    }
+
+    @GetMapping("/api/lenders/{id}")
+    Optional<Lender> getLender(@PathVariable long id) {
+        return lenderRepo.findById(id);
+    }
+
+    @PostMapping("/api/lenders")
+    Lender createLender(@RequestBody Lender lender) {
+        return lenderRepo.save(lender);
+    }
+
+    @PutMapping("/api/lenders/{id}")
+    Optional<Lender> updateLender(@RequestBody Lender lenderRequest, @PathVariable long id) {
+        Optional<Lender> opt = lenderRepo.findById(id);
+        if (opt.isPresent()) {
+            if (lenderRequest.getId() == id) {
+                return Optional.of(lenderRepo.save(lenderRequest));
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Erro ao alterar dados do lender com id " + id);
+    }
+
+    @DeleteMapping(value = "/api/lenders/{id}")
+    void deleteLender(@PathVariable long id) {
+        lenderRepo.deleteById(id);
+    }
+}
