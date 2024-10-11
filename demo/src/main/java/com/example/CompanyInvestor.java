04e9b26828073c5e12e119173109581e8ad4b200
Amanda Massari <54778522+amandaskywalker@users.noreply.github.com>
2024-10-11 01:21:59 +0000

    fase1

---
+++ demo/src/main/java/com/example/CompanyInvestor.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@DiscriminatorValue("INTERNAL")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CompanyInvestor extends Investor {
+  private String companyType;
+
+  @Column(unique = true)
+  private String lei;
+}
---
+++ demo/src/main/java/com/example/Investor.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype", discriminatorType = DiscriminatorType.STRING)
+@Data
+@EqualsAndHashCode(callSuper = true)
+public abstract class Investor extends AbstractEntity {
+  private String name;
+}
